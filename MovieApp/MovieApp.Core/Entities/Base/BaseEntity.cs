bf2f14f11ebfb30501c648127da46006099567f7
dexSamir <samirah-bp215@code.edu.az>
2025-01-07 18:25:42 +0400

    feat:architecture

---
+++ MovieApp/MovieApp.Core/Entities/Base/BaseEntity.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MovieApp.Core.Entities.Base;
+
+public class BaseEntity
+{
+    public int Id{ get; set; }
+    public DateTime CreatedTime { get; set; }
+    public bool IsDeleted { get; set; }
+}
