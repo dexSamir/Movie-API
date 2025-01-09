5c9161fe85095350907e6d77e190362c65b7a3c2
dexSamir <samirah-bp215@code.edu.az>
2025-01-10 03:48:10 +0400

    add references and install nugget packages

--- MovieApp/MovieApp.BL/obj/Debug/net8.0/MovieApp.BL.AssemblyInfo.cs
+++ MovieApp/MovieApp.BL/obj/Debug/net8.0/MovieApp.BL.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MovieApp.BL")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+28ef1c0334c9491881b8a7d3db7169afd335441a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7e45521f0ba11ff4f6e64b7661e6090c7de2b7ab")]
 [assembly: System.Reflection.AssemblyProductAttribute("MovieApp.BL")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MovieApp.BL")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
