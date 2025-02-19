0bf22f577dee6881af2316093f147c22ad1c0111
dexSamir <samirah-bp215@code.edu.az>
2025-02-19 23:06:03 +0400

    some updates

---
+++ MovieApp/MovieApp.DAL/Migrations/20250219181852_UdateUserTable.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MovieApp.DAL.Migrations
+{
+    /// <inheritdoc />
+    public partial class UdateUserTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Description",
+                table: "WatchRooms",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<int>(
+                name: "MaxParticipants",
+                table: "WatchRooms",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "ProfileUrl",
+                table: "AspNetUsers",
+                type: "text",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "text");
+
+            migrationBuilder.AddColumn<string>(
+                name: "ConfirmationToken",
+                table: "AspNetUsers",
+                type: "text",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Description",
+                table: "WatchRooms");
+
+            migrationBuilder.DropColumn(
+                name: "MaxParticipants",
+                table: "WatchRooms");
+
+            migrationBuilder.DropColumn(
+                name: "ConfirmationToken",
+                table: "AspNetUsers");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "ProfileUrl",
+                table: "AspNetUsers",
+                type: "text",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "text",
+                oldNullable: true);
+        }
+    }
+}
