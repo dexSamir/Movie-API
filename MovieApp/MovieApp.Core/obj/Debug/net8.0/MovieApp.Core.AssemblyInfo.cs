8e3c7d1a648ed8ffd1621673289caada8fe38c06
dexSamir <samirah-bp215@code.edu.az>
2025-03-02 21:13:31 +0400

    .

--- MovieApp/MovieApp.Core/obj/Debug/net8.0/MovieApp.Core.AssemblyInfo.cs
+++ MovieApp/MovieApp.Core/obj/Debug/net8.0/MovieApp.Core.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MovieApp.Core")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+418e65f7bff2c675ff35ed49588d1d6879e1c028")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+28d64e1159237f6ceacac225c9b933fa91b1c4c9")]
 [assembly: System.Reflection.AssemblyProductAttribute("MovieApp.Core")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MovieApp.Core")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
